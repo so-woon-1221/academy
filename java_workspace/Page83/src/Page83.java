f6653a2c0000f89ea3246263bf017dc5177bd42f
so-woon-1221 <janus7359@gmail.com>
2021-03-05 14:43:47 +0900

    증감 연산자 연습

---
+++ java_workspace/Page83/src/Page83.java
@@ -0,0 +1,13 @@
+public class Page83 {
+    public static void main(String[] args) {
+        int a = 1;
+
+        //a가 먼저 반환되고 증가가
+//        System.out.println(a++);
+//        System.out.println(a);
+
+        //a가 먼저 증가되고 반환
+        System.out.println(++a);
+        System.out.println(a);
+    }
+}
