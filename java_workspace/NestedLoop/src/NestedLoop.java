ff055c4b5f4e9387645c8e470f0f1be23ef000b5
so-woon-1221 <janus7359@gmail.com>
2021-03-08 14:46:04 +0900

    nestedloop 수정 - 가로로배열 / 세로로 배열

--- java_workspace/NestedLoop/src/NestedLoop.java
+++ java_workspace/NestedLoop/src/NestedLoop.java
@@ -1,8 +1,17 @@
 public class NestedLoop {
     public static void main(String[] args) {
+        //가로로 한 단씩
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
                 System.out.print(i + " * " + j + " = " + i * j + "\t");
+            }
+            System.out.println();
+        }
+
+        //세로로 한 단씩
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.print(j + " * " + i + " = " + i * j + "\t");
             }
             System.out.println();
         }
