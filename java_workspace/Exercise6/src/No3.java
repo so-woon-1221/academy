e7cd0911dd6b2eee32ccbd7a14e2be5a689994c4
so-woon-1221 <janus7359@gmail.com>
2021-03-10 18:05:26 +0900

    연습문제 6번 풀이

--- java_workspace/Exercise6/src/No3.java
+++ java_workspace/Exercise6/src/No3.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
+class Triangle {
+    Scanner scanner = new Scanner(System.in);
+    int base;
+    int height;
+
+    public void setBase() {
+        System.out.print("밑변의 길이를 입력하세요 : ");
+        this.base = scanner.nextInt();
+    }
+
+    public void setHeight() {
+        System.out.print("높이를 입력하세요 : ");
+        this.height = scanner.nextInt();
+    }
+
+    public double getArea() {
+        return 0.5 * base * height;
+    }
+}
 public class No3 {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+
+        triangle.setBase();
+        triangle.setHeight();
+        System.out.println("삼각형의 넓이는 " + triangle.getArea());
+    }
 }
