b17779e008717ceb6b960bf373093c021d0454ed
so-woon-1221 <janus7359@gmail.com>
2021-03-11 11:39:01 +0900

    static 예제 주석 추가

--- java_workspace/StaticSample/src/Ex.java
+++ java_workspace/StaticSample/src/Ex.java
@@ -14,11 +14,11 @@ class StaticSample {
 }
 public class Ex {
     public static void main(String[] args) {
-        StaticSample.m = 10;
+        StaticSample.m = 10; //m은 static이라 이렇게 접근 가능
 
         StaticSample s1;
         s1 = new StaticSample();
-        System.out.println(s1.m);
+        System.out.println(s1.m); //s1으로 접근도 가능하지만 그냥 StaticSample로도 접근가능
         s1.f();
         StaticSample.f();
         System.out.println(StaticSample.m);
