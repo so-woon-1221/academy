ac1b725c985a55bcc6d3eb0ed58067410dcfbdf5
so-woon-1221 <janus7359@gmail.com>
2021-03-16 15:11:35 +0900

    바이너리 파일 고속 복사 주석 추가

--- java_workspace/BlockBinaryCopyEx/src/BlockBinaryCopyEx.java
+++ java_workspace/BlockBinaryCopyEx/src/BlockBinaryCopyEx.java
@@ -13,9 +13,9 @@ public class BlockBinaryCopyEx {
             FileOutputStream outputStream = new FileOutputStream(dest);
             byte[] buf = new byte[1024 * 10];
             while (true) {
-                int n = inputStream.read(buf);
-                outputStream.write(buf, 0, n);
-                if (n < buf.length)
+                int n = inputStream.read(buf);  //파일을 읽어서 buf에 저장한후 저장한 바이트 크기를 n에 리턴
+                outputStream.write(buf, 0, n); //buf에 있는 내용을 outputstream에 0부터 n바이트만큼 적음
+                if (n < buf.length) //읽은 데이터가 buffer의 최대 사이즈보다 작다면 다 읽은것이기 때문에 끝낸다.
                     break;
             }
             inputStream.close();
