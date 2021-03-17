9eb2ab2485879a4571bfee84fe94fafbb8990596
so-woon-1221 <janus7359@gmail.com>
2021-03-17 13:38:22 +0900

    ActionLister 예제 - 독립 클래스로 리스너 생성

---
+++ java_workspace/IndepClassListener/src/IndepClassListener.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class IndepClassListener extends JFrame {
+    public IndepClassListener() {
+        setTitle("Action 리스너 예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container container = getContentPane();
+        container.setLayout(new FlowLayout());
+        JButton button = new JButton("Action");
+        button.addActionListener(new MyActionListener());
+        container.add(button);
+
+        setSize(300, 150);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new IndepClassListener();
+    }
+}
+
+class MyActionListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton) e.getSource();
+        if (button.getText().equals("Action")) {
+            button.setText("액션");
+        } else {
+            button.setText("Action");
+        }
+    }
+}
