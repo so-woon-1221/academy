16f07ee608cf37b75e75cdcb24b48ab9270084b2
so-woon-1221 <janus7359@gmail.com>
2021-03-23 16:09:10 +0900

    학사관리 프로그램 로그인 버튼 기능 수정 및 등록시 리스트에 표시하기 구현

--- java_workspace/Haksa/src/Haksa.java
+++ java_workspace/Haksa/src/Haksa.java
@@ -17,6 +17,8 @@ public class Haksa extends JFrame {
     JButton deleteButton = null;
     JButton loginButton;
 
+    Boolean isLogin = false;
+
     public Haksa() {
         this.setTitle("학사관리");
         this.setDefaultCloseOperation(3);
@@ -29,7 +31,7 @@ public class Haksa extends JFrame {
 
         JToolBar toolBar = new JToolBar("로그인");
         loginButton = new JButton("로그인");
-        loginButton.addActionListener(new loginActionListener(loginButton));
+        loginButton.addActionListener(new loginActionListener(loginButton, isLogin));
         toolBar.add(loginButton);
 
         c.add(toolBar, BorderLayout.NORTH);
@@ -78,6 +80,11 @@ public class Haksa extends JFrame {
 
                 if (errorMessage.equals("")) {
                     JOptionPane.showMessageDialog(null, "등록되었습니다.", "확인", JOptionPane.PLAIN_MESSAGE);
+                    taList.append(number + " " + name + " " + department + " " + address);
+                    tfName.setText("");
+                    tfId.setText("");
+                    tfDepartment.setText("");
+                    tfAddress.setText("");
                 } else {
                     errorMessage += "빈칸입니다.";
                     JOptionPane.showMessageDialog(null, errorMessage, "경고", JOptionPane.ERROR_MESSAGE);
@@ -112,56 +119,65 @@ public class Haksa extends JFrame {
 
     class loginActionListener implements ActionListener {
         JButton button;
+        Boolean isLogin;
 
-        public loginActionListener(JButton button) {
+        public loginActionListener(JButton button, Boolean isLogin) {
             this.button = button;
+            this.isLogin = isLogin;
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            JDialog dialog = new JDialog();
-            dialog.setTitle("로그인");
-            dialog.setSize(200, 200);
+            if (!isLogin) {
+                JDialog dialog = new JDialog();
+                dialog.setTitle("로그인");
+                dialog.setSize(200, 200);
 
-            Container container = dialog.getContentPane();
-            container.setLayout(new BorderLayout());
+                Container container = dialog.getContentPane();
+                container.setLayout(new BorderLayout());
 
-            JPanel fieldPanel = new JPanel();
-            fieldPanel.setLayout(new GridLayout(2, 2));
-            JLabel idLabel = new JLabel("아이디");
-            JTextField idField = new JTextField();
-            JLabel pwdLabel = new JLabel("비밀번호");
-            JPasswordField pwdField = new JPasswordField();
-            fieldPanel.add(idLabel);
-            fieldPanel.add(idField);
-            fieldPanel.add(pwdLabel);
-            fieldPanel.add(pwdField);
-            container.add(fieldPanel, BorderLayout.CENTER);
+                JPanel fieldPanel = new JPanel();
+                fieldPanel.setLayout(new GridLayout(2, 2));
+                JLabel idLabel = new JLabel("아이디");
+                JTextField idField = new JTextField();
+                JLabel pwdLabel = new JLabel("비밀번호");
+                JPasswordField pwdField = new JPasswordField();
+                fieldPanel.add(idLabel);
+                fieldPanel.add(idField);
+                fieldPanel.add(pwdLabel);
+                fieldPanel.add(pwdField);
+                container.add(fieldPanel, BorderLayout.CENTER);
 
-            JPanel buttonPanel = new JPanel();
-            buttonPanel.setLayout(new GridLayout(1, 2));
-            JButton loginButton = new JButton("로그인");
-            loginButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    JOptionPane.showMessageDialog(null, "[ " + idField.getText() + " ]로그인 완료", "로그인",
-                            JOptionPane.PLAIN_MESSAGE);
-                    dialog.setVisible(false);
-                    button.setText("로그아웃");
-                }
-            });
-            JButton cancelButton = new JButton("취소");
-            cancelButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    dialog.setVisible(false);
-                }
-            });
-            buttonPanel.add(loginButton);
-            buttonPanel.add(cancelButton);
-            container.add(buttonPanel, BorderLayout.SOUTH);
+                JPanel buttonPanel = new JPanel();
+                buttonPanel.setLayout(new GridLayout(1, 2));
+                JButton loginButton = new JButton("로그인");
+                loginButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        JOptionPane.showMessageDialog(null, "[ " + idField.getText() + " ]로그인 완료", "로그인",
+                                JOptionPane.PLAIN_MESSAGE);
+                        dialog.setVisible(false);
+                        button.setText("로그아웃");
+                        isLogin = true;
+                    }
+                });
+                JButton cancelButton = new JButton("취소");
+                cancelButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        dialog.setVisible(false);
+                    }
+                });
+                buttonPanel.add(loginButton);
+                buttonPanel.add(cancelButton);
+                container.add(buttonPanel, BorderLayout.SOUTH);
 
-            dialog.setVisible(true);
+                dialog.setVisible(true);
+            } else {
+                JOptionPane.showMessageDialog(null, "로그아웃했습니다.", "로그아웃", JOptionPane.ERROR_MESSAGE);
+                button.setText("로그인");
+                isLogin = false;
+            }
         }
     }
 
