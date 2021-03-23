e696065746afa00d6b4be89eeba245df5be093a5
so-woon-1221 <janus7359@gmail.com>
2021-03-23 11:35:58 +0900

    학사관리 프로그램 수정

--- java_workspace/Haksa/src/Haksa.java
+++ java_workspace/Haksa/src/Haksa.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Haksa extends JFrame {
     JTextField tfId = null;
@@ -21,29 +23,53 @@ public class Haksa extends JFrame {
         Container container = this.getContentPane();
         container.setLayout(new FlowLayout());
 
-        tfId = new JTextField(20);
+        tfId = new JTextField(25);
         container.add(new JLabel("학번"));
         container.add(tfId);
 
         container.add(new JLabel("이름"));
-        tfName = new JTextField(20);
+        tfName = new JTextField(25);
         container.add(tfName);
 
         container.add(new JLabel("학과"));
-        tfDepartment = new JTextField(20);
+        tfDepartment = new JTextField(25);
         container.add(tfDepartment);
 
         container.add(new JLabel("주소"));
-        tfAddress = new JTextField(20);
+        tfAddress = new JTextField(25);
         container.add(tfAddress);
 
-        taList = new JTextArea(10, 23);
+        taList = new JTextArea(10, 28);
         container.add(new JScrollPane(taList));
 
         insertButton = new JButton("등록");
+        insertButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "등록되었습니다.");
+            }
+        });
         container.add(insertButton);
 
-        this.setSize(300, 500);
+        selectButton = new JButton("목록");
+        container.add(selectButton);
+
+        updateButton = new JButton("수정");
+        container.add(updateButton);
+
+        deleteButton = new JButton("삭제");
+        container.add(deleteButton);
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int result = JOptionPane.showConfirmDialog(null, "삭제하시겠습니까?", "삭제", JOptionPane.YES_NO_OPTION);
+                if (result == JOptionPane.YES_OPTION) {
+                    //삭제처리
+                }
+            }
+        });
+
+        this.setSize(350, 500);
         this.setVisible(true);
     }
 
