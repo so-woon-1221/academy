bf5298385b664ca1713b84a9cf58861ad2c3e2e1
so-woon-1221 <janus7359@gmail.com>
2021-03-24 23:58:34 +0900

    쓰레드를 이용한 양방향 채팅 프로그램 및 gui로 닉네임 받아서 보내기

---
+++ java_workspace/ThreadChatting/src/ChatServer.java
@@ -0,0 +1,106 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatServer extends JFrame {
+    JTextArea chatArea;
+    JTextField chatField;
+
+    BufferedWriter writer;
+    BufferedReader reader;
+    ServerSocket serverSocket;
+    Socket socket;
+    Scanner scanner;
+
+    String nick;
+
+    public ChatServer() {
+        this.setTitle("채팅 - 서버");
+        this.setSize(400, 400);
+
+        Container container = this.getContentPane();
+        container.setLayout(new BorderLayout());
+
+        JToolBar toolBar = new JToolBar();
+        JTextField id = new JTextField(10);
+        JButton login = new JButton("로그인");
+        login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                nick = id.getText();
+                chatArea.append(nick + "으로 로그인했습니다." + "\n");
+                id.setText("");
+            }
+        });
+        toolBar.add(id);
+        toolBar.add(login);
+
+        container.add(toolBar, BorderLayout.NORTH);
+
+        chatArea = new JTextArea();
+        container.add(chatArea, BorderLayout.CENTER);
+
+        chatField = new JTextField();
+        container.add(chatField, BorderLayout.SOUTH);
+
+        this.setVisible(true);
+
+        try {
+            serverSocket = new ServerSocket(9999);
+            chatArea.append("대기중" + "\n");
+            socket = serverSocket.accept();
+            chatArea.append("연결완료" + "\n");
+            scanner = new Scanner(System.in);
+
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        chatField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = chatField.getText();
+                chatArea.append(nick + "(본인) : " + message + "\n");
+                chatField.setText("");
+
+                try {
+                    writer.write(nick + " : " + message + "\n");
+                    writer.flush();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
+
+        ReadThread readThread = new ReadThread();
+        readThread.start();
+    }
+
+    class ReadThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    String inputMessage = reader.readLine();
+                    chatArea.append(inputMessage + "\n");
+                }
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+}
