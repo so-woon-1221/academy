7acec6805441949323932f838383dc26d7e8e499
so-woon-1221 <janus7359@gmail.com>
2021-04-01 15:21:09 +0900

    라이브러리 예제

---
+++ java_workspace/LibraryTest/src/LibarayTest.java
@@ -0,0 +1,8 @@
+public class LibarayTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Sample sample = new Sample();
+        sample.name = "ai";
+        System.out.println(sample.getName());
+    }
+}
