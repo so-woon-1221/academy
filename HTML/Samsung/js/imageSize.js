df6f5919d29656efb828100f62fc1448326e2177
so-woon-1221 <janus7359@gmail.com>
2021-04-21 15:49:51 +0900

    삼성사이트 카피

---
+++ HTML/Samsung/js/imageSize.js
@@ -0,0 +1,42 @@
+let desktops = document.querySelectorAll('.desktop');
+let mobiles = document.querySelectorAll('.mobile');
+
+window.onload = () => {
+    if (window.innerWidth <= 768) {
+        for (let desktop of desktops) {
+            desktop.style.display = 'none';
+        }
+
+        for (let mobile of mobiles) {
+            mobile.style.display = 'block';
+        }
+    }
+}
+
+for (let desktop of desktops) {
+    desktop.style.display = 'block';
+}
+
+for (let mobile of mobiles) {
+    mobile.style.display = 'none';
+}
+
+window.addEventListener('resize', ev => {
+    if (window.innerWidth <= 768) {
+        for (let desktop of desktops) {
+            desktop.style.display = 'none';
+        }
+
+        for (let mobile of mobiles) {
+            mobile.style.display = 'block';
+        }
+    } else {
+        for (let desktop of desktops) {
+            desktop.style.display = 'block';
+        }
+
+        for (let mobile of mobiles) {
+            mobile.style.display = 'none';
+        }
+    }
+})
